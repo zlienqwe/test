24ca5044cdc4f81e9d0686a2ee71c2b07cec4fcf
Zlien <Zlien@bogon.twguest.thoughtworks.com>
2015-09-14 14:33:21 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Zlien on 9/14/15.
+//  Copyright (c) 2015 Zlien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Zlien on 9/14/15.
+//  Copyright (c) 2015 Zlien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
